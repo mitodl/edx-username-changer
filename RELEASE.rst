e9f3e208ca5c785c2761df274098a59688a7b388
Doof <mitx-devops@mit.edu>
2021-04-28 20:29:39 +0000

    Release 0.1.0

---
+++ RELEASE.rst
@@ -0,0 +1,8 @@
+Release Notes
+=============
+
+Version 0.1.0
+-------------
+
+- replacing celery task with celery shared_task (#3)
+
--- edx_username_changer/__init__.py
+++ edx_username_changer/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 default_app_config = "edx_username_changer.apps.EdxUsernameChangerConfig"
